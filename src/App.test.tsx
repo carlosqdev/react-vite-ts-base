8a4e3eb109d316f060f74944aa499deee5f6a7ec
carlosqdev <carlosquintero95nic@gmail.com>
2022-09-06 17:20:40 -0600

    Config jest as testrunner

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {act} from 'react-dom/test-utils';
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+describe('App', function () {
+  test('should first', () => { 
+    console.log("hola mundo")
+   })
+});
